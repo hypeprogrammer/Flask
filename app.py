96cd04c28d1710727cbd5d15ce2096e8a1f86620
hypeprogrammer <156506543+hypeprogrammer@users.noreply.github.com>
2024-03-31 22:57:16 +0900

    flask 게시판 : 회원가입 라우트

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
 from models import User
+from werkzeub.security import generate_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ''
@@ -20,4 +20,16 @@ if __name__ == "__main__":
 @login_manager.user_loader
 def load_user(user_id):
 	return User.query.get(int(user_id))
+
+@app.route("/signup", methods=["GET", "POST"])
+def sighup():
+	form = RegistrationForm()
+	if form.validata_on_submit():
+		hashed_password = generate_password_hash(form.password.data)
+		new_user = User(username=form.username.data, password=hashed_password)
+		db.session.add(new_user)
+		db.session.commit()
+		flash('계정생성완료')
+		return redirect(url_for('login'))
+	return render_template('signup.html', title='Sign Up', form=form)
 	
