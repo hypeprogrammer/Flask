b169480851f010bde0bcf071ac551405a0e60b71
hypeprogrammer <156506543+hypeprogrammer@users.noreply.github.com>
2024-03-31 22:27:29 +0900

    flask 게시판 : user_id 모델 추가

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+from models import User
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = ''
@@ -14,4 +16,8 @@ login_manager.init_app(app)
 
 if __name__ == "__main__":
 	app.run(debug=True)
+	
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 	
